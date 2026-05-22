71bbb08ddaa8dbf6a4c6abd0fe0cfe82a6e4548d
Luiz Carlos Orlandi Junior <orandiluiz@gmail.com>
2026-05-21 21:02:43 -0300

    Feito a criação dos modulos Auth e User com conceitos no seu estagio inicial atualmente de SOLID (Soc e SRP)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
