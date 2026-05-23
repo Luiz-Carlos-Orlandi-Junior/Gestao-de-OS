c6af3182aee182066d3afcf7b67bfd8427f51f4b
Luiz Carlos Orlandi Junior <orandiluiz@gmail.com>
2026-05-22 22:05:00 -0300

    Prisma instalado e configurado com o banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersService } from './users/users.service';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
+import { AuthModule } from 'auth/auth.module';
+import { UserModule } from 'user/user.module';
+
 
 @Module({
   imports: [AuthModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService, UsersService],
 })
 export class AppModule {}
