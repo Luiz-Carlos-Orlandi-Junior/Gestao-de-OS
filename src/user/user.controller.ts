50cedf8ea69f0a4d5d7168541deafff71ac6e76a
Luiz Carlos Orlandi Junior <orandiluiz@gmail.com>
2026-06-18 19:52:32 -0300

    Testado  e finalizado o CRUD Users e conexão com o Banco de dados

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,21 +1,43 @@
-import { Controller, Body, Post} from '@nestjs/common';
-import { User as UserModel } from 'generated/prisma';
+import { Controller, Body, Post, Get, Patch, Delete, Param } from '@nestjs/common';
+import { User as UserModel, Prisma } from '@prisma/client'; // ← adicione o Prisma aqui
 import { UserService } from './user.service';
-
 
 @Controller('user')
 export class UserController {
-  constructor(
-    private readonly userService: UserService
-  ) {}
-
+  constructor(private readonly userService: UserService) {}
 
   @Post('signup')
   async signupUser(
-    @Body() userData: { name: string; email: string, password: string},
+    @Body() userData: { name: string; email: string; password: string },
   ): Promise<UserModel> {
     return this.userService.createUser(userData);
   }
 
+  @Get(':id')
+  async getUser(@Param('id') id: string): Promise<UserModel | null> {
+    return this.userService.user({ id: Number(id) });
+  }
 
+  @Get()
+async getUsers(): Promise<UserModel[]> {
+  return this.userService.users({});
 }
+
+  @Patch(':id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body() userData: Prisma.UserUpdateInput,
+  ): Promise<UserModel> {
+    return this.userService.updateUser({
+      where: { id: Number(id) },
+      data: userData,
+    });
+  }
+
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.deleteUser({ id: Number(id) });
+  }
+}
+
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,7 @@
 
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../database/prisma.service';
-import { User, Prisma } from 'generated/prisma';
+import { User, Prisma } from '@prisma/client';
 
 @Injectable()
 export class UserService {
